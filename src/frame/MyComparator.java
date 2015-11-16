70bd2e1030847462fe50fc968a1bb0703f7cba20
Yurii-Kudlay <yurgen_2014@ukr.net>
2015-11-16 19:30:26 +0200

    test

---
+++ src/frame/MyComparator.java
@@ -0,0 +1,22 @@
+ package com.sql.jdbc.frame;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator{
+
+	
+	@Override
+public int compare(Object o1, Object o2) {
+	Double i1 = Double.valueOf(o1.toString()).doubleValue();
+	Double i2 = Double.valueOf(o2.toString()).doubleValue();
+	
+	if (i1 < i2){
+		return 1;
+	}else if (i1 > i2){
+		return -1;
+	}else {
+		return 0;
+	}
+
+}
+}
